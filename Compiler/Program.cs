9c1d775ea086762557be706c46daaf33bdea73fc
Florian Schwabe <florian.schwabe@hotmail.de>
2019-12-10 20:42:49 +0100

    Change file extension of compiled files

--- Compiler/Program.cs
+++ Compiler/Program.cs
@@ -3,20 +3,22 @@ using System.IO;
 using AocIntComputer.Compiler;
 
 namespace Compiler {
-    class Program {
-        public static void Main(string[] args) {
-            /*
-             * Options:
-             *     --binary - to binary file
-             */
+    static class Program {
+        private static string FileNameWithoutExtension(this FileInfo file) {
+            return file.FullName.Substring(
+                0,
+                file.FullName.LastIndexOf(".", StringComparison.Ordinal)
+            );
+        }
 
+        public static void Main(string[] args) {
             FileInfo fileIn = new FileInfo(args[0]);
 
             if (!fileIn.Exists) {
                 Console.WriteLine($"File \"{args[0]}\" does not exist");
             }
 
-            FileInfo fileOut = new FileInfo(fileIn.Name + ".INT");
+            FileInfo fileOut = new FileInfo(fileIn.FileNameWithoutExtension() + ".INTCOMPUTER");
 
             long[] compiledProgram = new IntComputerCompiler(fileIn.OpenText().ReadToEnd()).Compile();
 
